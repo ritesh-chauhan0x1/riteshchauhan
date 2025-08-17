0630c829dda16f904e8f6e987ca412f0fa68e0fa
Ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 15:42:53 +0530

    Remove desktop-projects folder; now using external GitHub repos for desktop projects

--- desktop-projects/Discord-Clone/src/contexts/ServerContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ServerContext = React.createContext();
-
-export const ServerProvider = ServerContext.Provider;
-export const ServerConsumer = ServerContext.Consumer;
-
-export default ServerContext;
